b812cb8e8a7a8d37cb954f099848ff31610fad02
Avichai <tchjha2@gmail.com>
2025-07-03 00:23:25 +0300

    Add Azure credentials as environment variables for Terraform Apply

--- variables.tf
+++ variables.tf
@@ -1,13 +1,21 @@
 variable "subscription_id" {
   description = "The Azure subscription ID where resources will be created."
   type        = string
-  default     = "63e736f9-c77b-48e5-aeaf-853e4c7cc4d1"
 }
 
 variable "tenant_id" {
   description = "The Azure tenant ID for authentication."
   type        = string
-  default     = "79caa7d1-da42-47a2-b507-432ca23f0704"
+}
+
+variable "client_id" {
+  description = "Service Principal Client ID"
+  type        = string
+}
+
+variable "client_secret" {
+  description = "Service Principal Client Secret"
+  type        = string
 }
 
 variable "resource_group_name" {
